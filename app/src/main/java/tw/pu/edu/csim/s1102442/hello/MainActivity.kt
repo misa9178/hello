0d8f36e44f6b94e2351a8f4d4c1c4c3b15dbce7c
芦田紗羽 <sawaas1230@gmail.com>
2023-04-18 14:25:53 +0800

    第一次Commit

---
+++ app/src/main/java/tw/pu/edu/csim/s1102442/hello/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.pu.edu.csim.s1102442.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
